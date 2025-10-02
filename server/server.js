5fa61d96a21a27973232bc9cfcaf793b4939985f
Alex <aluciencozy22@gmail.com>
2025-10-02 12:32:27 -0400

    set up cors to test website

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import watchlistsRouter from './routes/watchlists.routes.js';
 import authRouter from './routes/auth.routes.js';
@@ -10,6 +11,8 @@ import errorMiddleware from './middlewares/error.middleware.js';
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
