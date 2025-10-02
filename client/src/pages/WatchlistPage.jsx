a06dd42324b8c2598b0be8f6b60ce0698dac9147
Alex <aluciencozy22@gmail.com>
2025-10-02 17:26:35 -0400

    sets up watchlist page

--- client/src/pages/WatchlistPage.jsx
+++ client/src/pages/WatchlistPage.jsx
@@ -1,6 +1,29 @@
-import AnimeCard from '../components/AnimeCard.jsx'
+import AnimeCard from '../components/AnimeCard.jsx';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BeatLoader } from 'react-spinners';
+
+const watchlistDetailQuery = `
+  query ($ids: [Int]) {
+    Page {
+      media(id_in: $ids, type: ANIME) {
+        id
+        title {
+          romaji
+          english
+          native
+        }
+        averageScore
+        format
+        episodes
+        duration
+        coverImage {
+          large
+        }
+      }
+    }
+  }
+`;
 
 const WatchlistPage = () => {
   const [watchlist, setWatchlist] = useState([]);
@@ -14,13 +37,32 @@ const WatchlistPage = () => {
     const token = localStorage.getItem('token');
 
     try {
-      const response = await axios.get('http://localhost:5001/api/v1/watchlists', {
+      const response = await axios.get('http://localhost:5001/api/v1/watchlist', {
         headers: {
           'Authorization': `Bearer ${token}`
         }
       })
 
-      setWatchlist(response.data.data.watchlist);
+      const watchlistItems = response.data.data.watchlist;
+      const animeIds = watchlistItems.map(item => item.animeId);
+
+      const variables = {
+        ids: animeIds
+      };
+
+      const body = {
+        query: watchlistDetailQuery,
+        variables: variables
+      };
+
+      const response2 = await axios.post('https://graphql.anilist.co', body, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      });
+
+      setWatchlist(response2.data.data.Page.media);
     } catch (error) {
       console.error(`Error fetching watchlist: ${error}`);
       setErrorMessage('Error fetching watchlist. Please try again later.');
@@ -34,9 +76,37 @@ const WatchlistPage = () => {
   }, []);
 
   return (
-    <main>
-      <h1 className="text-2xl text-white absolute z-50">My Watchlist</h1>
-      {/* map AnimeCards here */}
+    <main className="h-screen w-screen pt-[100px]">
+      <div className="flex gap-y-14 gap-x-7 lg:gap-x-10 xl:gap-x-14 2xl:gap-x-18 justify-items-center pb-14 px-7 lg:px-10 xl:px-14 2xl:px-18">
+        {isLoading ? (
+          <div className="">
+            <BeatLoader color="#3ac86a" />
+          </div>
+        ) : errorMessage ? (
+          <p className="text-red-500 md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
+            {errorMessage}
+          </p>
+        ) : (
+          watchlist.map((anime) => {
+            if (!anime.episodes) return null;
+            return (
+              <AnimeCard
+                key={anime.id}
+                title={
+                  anime.title.english
+                    ? anime.title.english
+                    : anime.title.romaji
+                }
+                rating={anime.averageScore}
+                image={anime.coverImage.large}
+                format={anime.format}
+                episodes={anime.episodes}
+                duration={anime.duration}
+              />
+            );
+          })
+        )}
+      </div>
     </main>
   );
 };
