94225b4b301cb5ed0c4a6538ff613f5f7c059d8c
Alex <aluciencozy22@gmail.com>
2025-10-04 16:04:54 -0400

    styles sign in/up pages

--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -39,17 +39,28 @@ const SignInPage = () => {
 
   return (
     <main className="w-full h-screen flex justify-center items-center text-white">
-      <div className="flex flex-col items-center bg-(--color-bg-secondary) h-[400px] w-[400px] rounded-lg shadow-lg p-10">
-        <form onSubmit={handleSubmit} className="h-full w-full flex flex-col justify-center">
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" value={email} onChange={handleChange} required className="bg-(--color-bg) mb-5" />
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" value={password} onChange={handleChange} required className="bg-(--color-bg) mb-5" />
-          <button type="submit" className="bg-(--color-primary) hover:bg-(--color-primary-hover) text-white font-bold py-2 px-4 rounded cursor-pointer transition-colors duration-200">Sign In</button>
+      <div className="flex flex-col items-center bg-(--color-bg-secondary)/60 h-[500px] w-[500px] shadow-lg rounded-sm p-12">
+        <div className="relative">
+          <h1 className="text-3xl font-bold mb-5 text-shadow-lg after:h-0.5 after:w-[180%] after:absolute after:bottom-2 after:left-1/2 after:-translate-x-1/2 after:bg-(--color-primary) after:shadow-xl">
+            <span className="bg-clip-text text-transparent bg-linear-to-r from-(--color-secondary) to-(--color-primary)">
+            Sign
+            </span> In
+          </h1>
+        </div>
+        <form onSubmit={handleSubmit} className="h-full w-full flex flex-col gap-y-5 justify-center">
+          <div className="relative flex flex-col w-full after:w-full after:h-0.5 after:bg-(--color-primary) after:absolute after:bottom-0 after:left-0 after:transition-all after:duration-300 after:content-[''] after:scale-x-0 after:origin-center focus-within:after:scale-x-100">
+            <input type="email" id="email" name="email" value={email} placeholder=" " onChange={handleChange} required className="input-form peer focus:outline-none" />
+            <label htmlFor="email" className="label-form peer-placeholder-shown:scale-100 peer-focus:scale-60 peer-focus:-translate-y-7 peer-focus:translate-x-[-20%] peer-[:not(:placeholder-shown)]:scale-60 peer-[:not(:placeholder-shown)]:-translate-y-7 peer-[:not(:placeholder-shown)]:translate-x-[-20%] peer-focus:text-gray-500">Email</label>
+          </div>
+          <div className="relative flex flex-col w-full after:w-full after:h-0.5 after:bg-(--color-primary) after:absolute after:bottom-0 after:left-0 after:transition-all after:duration-300 after:content-[''] after:scale-x-0 after:origin-center focus-within:after:scale-x-100">
+            <input type="password" id="password" name="password" value={password} placeholder=" " onChange={handleChange} required className="peer input-form focus:outline-none" />
+            <label htmlFor="password" className="label-form peer-placeholder-shown:scale-100 peer-focus:scale-60 peer-focus:-translate-y-7 peer-focus:translate-x-[-20%] peer-[:not(:placeholder-shown)]:scale-60 peer-[:not(:placeholder-shown)]:-translate-y-7 peer-[:not(:placeholder-shown)]:translate-x-[-20%] peer-focus:text-gray-500 peer-[:not(:placeholder-shown)]:text-gray-500">Password</label>
+          </div>
+          <button type="submit" className="bg-(--color-primary) hover:bg-(--color-primary)/20 hover:text-(--color-primary) text-white font-bold py-2 px-4 rounded cursor-pointer transition-colors duration-200 mt-5">Sign In</button>
         </form>
-        <div className="flex flex-col justify-center items-center">
-          <p className="text-white">Don't have an account?</p>
-          <Link to="/sign-up" className="bg-(--color-primary) hover:bg-(--color-primary-hover) text-white font-bold py-2 px-4 rounded cursor-pointer transition-colors duration-200">Sign Up</Link>
+        <div className="flex gap-x-2 justify-center items-center">
+          <p className="text-white">Not registered?</p>
+          <Link to="/sign-up" className="text-(--color-primary) hover:text-(--color-secondary) transition-colors duration-300">Sign Up</Link>
         </div>
       </div>
     </main>
